d5ca4b665e96d3e6c013888a054aa61a15d54357
theflyginwhale <greblavelo@gmail.com>
2022-07-19 21:51:22 +0200

    remove height that ruins scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,52 @@
-import { Heading, VStack, HStack, Text } from "@chakra-ui/react";
-import { useGameContext } from "./context/GameContext";
-import { WorldPanel } from "./components/worldPanel";
-import { TilePanel } from "./components/tilePanel";
-import { PlayerPanel } from "./components/playerPanel";
-import { EnemyPanel } from "./components/enemyPanel";
-import { DeveloperControlPanel } from "./components/developerControlPanel";
+import { Heading, VStack, HStack, Text } from '@chakra-ui/react';
+import { useGameContext } from './context/GameContext';
+import { WorldPanel } from './components/worldPanel';
+import { TilePanel } from './components/tilePanel';
+import { PlayerPanel } from './components/playerPanel';
+import { EnemyPanel } from './components/enemyPanel';
+import { DeveloperControlPanel } from './components/developerControlPanel';
 
 function App() {
-    const { gameState } = useGameContext();
+	const { gameState } = useGameContext();
 
-    return (
-        <VStack
-            maxW="full"
-            h="full"
-            bg="gray.100"
-            justifyContent="center"
-            p={25}
-        >
-            <Heading>Inter Mundos - React & Typescript</Heading>
-            <HStack>
-                <Text>
-                    <b>Game Name:</b> {gameState.name}
-                </Text>
-                <Text>
-                    <b>Game round:</b> {gameState.round}
-                </Text>
-                <Text>
-                    <b>Game over:</b>{" "}
-                    {!gameState.gameOver ? "Game" : "Game Over"}
-                </Text>
-            </HStack>
+	return (
+		<VStack
+			maxW="full"
+			bg="gray.100"
+			justifyContent="center"
+			p={25}
+			overflow="scroll"
+		>
+			<Heading>Inter Mundos - React & Typescript</Heading>
+			<HStack>
+				<Text>
+					<b>Game Name:</b> {gameState.name}
+				</Text>
+				<Text>
+					<b>Game round:</b> {gameState.round}
+				</Text>
+				<Text>
+					<b>Game over:</b> {!gameState.gameOver ? 'Game' : 'Game Over'}
+				</Text>
+			</HStack>
 
-            <VStack
-                bg={gameState.gameOver ? "red.500" : "gray.500"}
-                p={12}
-                borderRadius={12}
-                color="white"
-            >
-                <HStack>
-                    <WorldPanel />
-                    <PlayerPanel />
-                    <TilePanel />
-                    {gameState.enemy.name !== "placeholder" && <EnemyPanel />}
-                </HStack>
-            </VStack>
+			<VStack
+				bg={gameState.gameOver ? 'red.500' : 'gray.500'}
+				p={12}
+				borderRadius={12}
+				color="white"
+			>
+				<HStack>
+					<WorldPanel />
+					<PlayerPanel />
+					<TilePanel />
+					{gameState.enemy.name !== 'placeholder' && <EnemyPanel />}
+				</HStack>
+			</VStack>
 
-            <DeveloperControlPanel />
-        </VStack>
-    );
+			<DeveloperControlPanel />
+		</VStack>
+	);
 }
 
 export default App;
